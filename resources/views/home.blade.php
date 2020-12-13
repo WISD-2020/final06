a6cb8ecbdc5fd8bea24d9b94876fd9bd5dbfb381
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-13 21:12:52 +0800

    修改圖片

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,38 +42,34 @@
 <!-- Banner -->
 <section class="banner full">
     <article>
-        <img src="images/slide01.jpg" alt="" />
+        <img src="images/twa-4.jpg" alt="" />
         <div class="inner">
             <header>
-                <p>A free responsive web site template by <a href="https://templated.co">TEMPLATED</a></p>
-                <h2>Hielo</h2>
+
             </header>
         </div>
     </article>
     <article>
-        <img src="images/slide02.jpg" alt="" />
+        <img src="images/twa-2.jpg" alt="" />
         <div class="inner">
             <header>
-                <p>Lorem ipsum dolor sit amet nullam feugiat</p>
-                <h2>Magna etiam</h2>
+
             </header>
         </div>
     </article>
     <article>
-        <img src="images/slide03.jpg"  alt="" />
+        <img src="images/twa-7.jpg"  alt="" />
         <div class="inner">
             <header>
-                <p>Sed cursus aliuam veroeros lorem ipsum nullam</p>
-                <h2>Tempus dolor</h2>
+
             </header>
         </div>
     </article>
     <article>
-        <img src="images/slide04.jpg"  alt="" />
+        <img src="images/3f-1.jpg"  alt="" />
         <div class="inner">
             <header>
-                <p>Adipiscing lorem ipsum feugiat sed phasellus consequat</p>
-                <h2>Etiam feugiat</h2>
+
             </header>
         </div>
     </article>
@@ -81,8 +77,7 @@
         <img src="images/slide05.jpg"  alt="" />
         <div class="inner">
             <header>
-                <p>Ipsum dolor sed magna veroeros lorem ipsum</p>
-                <h2>Lorem adipiscing</h2>
+
             </header>
         </div>
     </article>
