82d04d75751b60ea9f45c82cf79240899efae433
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-14 01:38:56 +0800

    product功能

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,7 +18,12 @@ Route::group([
     Route::get('/users/create',[UserController::class, 'create']);
     Route::get('/users/{id}/edit',[UserController::class, 'edit']);
     Route::get('/users/{id}',[UserController::class, 'show']);
+
     Route::get('/products',[ProductController::class, 'index']);
+    Route::get('/products/create',[ProductController::class, 'create']);
+    Route::post('/products','ProductController@store')->name('product.store');
+    Route::get('/products/{id}/edit',[ProductController::class, 'edit']);
+    Route::get('/products/{id}',[ProductController::class, 'show']);
 
 
 });
