77b40cc55aa036f8c76fb2c00cb12e317165a7f0
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-23 07:24:50 +0800

    建立刪除,新增,顯示留言

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\Message;
+use App\Repositories\MessageRepository;
+use App\Http\Controllers\Controller;
 
 class MessageController extends Controller
 {
-    public function __construct()
+    public function __construct(MessageRepository $messages)
     {
         $this->middleware('auth');
+        $this->messages = $messages;
     }
     public function index(Request $request)
     {
-        $messages = $request->user()->messages()->get();
-
+        //$messages = $request->user()->messages()->get();
+        $messages = Message::where('user_id', $request->user()->id)->get();
         return view('messages.index', [
             'messages' => $messages,
         ]);
@@ -23,14 +28,14 @@ class MessageController extends Controller
         $this->validate($request, [
             'name' => 'required|max:255',
         ]);
-
-        $request->user()->messages()->create([
-            'name' => $request->name,
-        ]);
+        $request->user()->messages()->create($request->all());
+//        $request->user()->messages()->create([
+//            'name' => $request->name,
+//        ]);
 
         return redirect('/messages');
     }
-    public function destroy(Request $request, Message $message)
+    public function destroy(Message $message)
     {
         $message->delete();
 
