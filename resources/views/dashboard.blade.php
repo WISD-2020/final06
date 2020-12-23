389e30a2e61604c061a3914bd27da04ddaf59aa7
li ya hsuan <ab53014698@yahoo.com.tw>
2020-12-24 00:32:02 +0800

    修改會員登入後呈現的view(dashboard)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,47 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard ') }} {{ __('Home') }} {{ __('Elements ') }}{{ __('Generic') }}
-        </h2>
+@extends('layouts.master')
+@section('title','會員')
+@section('content')
+<section id="one" class="wrapper style2">
+    <div class="inner">
+        <x-app-layout>
+            <x-slot name="header">
+            </x-slot>
 
-    </x-slot>
+        <div class="grid-style">
+            <div>
+                <div class="box">
+                    <div class="image fit">
+                        <img src="images/visit.jpg" alt="" />
+                    </div>
+                    <div class="content">
+                        <header class="align-center">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome /><!-- Nav -->
-
+                            <h2>預約參訪</h2>
+                        </header>
+                        <footer class="align-center">
+                            <a href="{{route('visits.index')}}" class="button alt">預約GO！</a>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="box">
+                    <div class="image fit">
+                        <img src="images/comment.jpg" alt="" />
+                    </div>
+                    <div class="content">
+                        <header class="align-center">
+                            <h2>留言板</h2>
+                        </header>
+                        <footer class="align-center">
+                            <a href="{{route('internal.index')}}" class="button alt">留言GO！</a>
+                        </footer>
+                    </div>
+                </div>
             </div>
         </div>
+        </x-app-layout>
     </div>
 
-</x-app-layout>
+</section>
+@endsection
