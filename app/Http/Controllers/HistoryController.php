a09875f14a1032caf97e13fe3cfbd88ad8823c60
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-23 22:11:55 +0800

    修改history

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\Message;
+use App\Repositories\MessageRepository;
+use App\Http\Controllers\Controller;
 
 class HistoryController extends Controller
 {
+
     public function index()
     {
+        //$messages = $request->user()->messages()->get();
+//        $messages = Message::where('user_id', $request->user()->id)->get();
+//        return view('messages.index', [
+//            'messages' => $messages,
+//        ]);
         return view('introduction/history');
     }
 }
