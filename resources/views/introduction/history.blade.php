d8a053abb81c952856485ccf2f405ceedf876f64
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-24 17:58:15 +0800

    修改歷史view

--- resources/views/introduction/history.blade.php
+++ resources/views/introduction/history.blade.php
@@ -19,13 +19,44 @@
                     <p>台灣美術館</p>
                     <h2>歷史介紹</h2>
                 </header>
+
                 <form action="">
                     <table align="center">
                         <tr>
+
                             <td width="15%" align="center">開始日期</td>
+
                             <td width="15%" align="center">結束日期</td>
+
                             <td width="15%" align="center">內容</td>
+
+
                         </tr>
+
+
+                        <!-- 顯示目前留言 -->
+
+{{--                                <div class="panel-body">--}}
+{{--                                    <table class="table table-striped message-table">--}}
+
+{{--                                        <!-- 表頭 -->--}}
+{{--                                        <thead>--}}
+{{--                                        <th>留言</th>--}}
+{{--                                        <th>&nbsp;</th>--}}
+{{--                                        </thead>--}}
+
+{{--                                        <!-- 表身 -->--}}
+{{--                                        <tbody>--}}
+{{--                                            <tr>--}}
+{{--                                                <!-- Message Name -->--}}
+{{--                                                <td class="table-text">--}}
+{{--                                                    <div>{{ $museum_histories->date }}</div>--}}
+
+{{--                                                </td>--}}
+{{--                                                <td class="table-text">--}}
+{{--                                                    <div>{{ $museum_histories->created_at }}</div>--}}
+{{--                                                </td>--}}
+
 {{--                        <tr>--}}
 {{--                            <td width="15%" align="center">{{Auth::museum_histories()->date}}</td>--}}
 {{--                            <td width="15%" align="center">{{Auth::museum_histories()->end_date}}</td>--}}
