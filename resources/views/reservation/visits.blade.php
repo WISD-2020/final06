11f6b0637909624c1d2d7ead5a312f9c4be99a37
li ya hsuan <ab53014698@yahoo.com.tw>
2020-12-24 23:40:36 +0800

    修改參訪(visits)views 2

--- resources/views/reservation/visits.blade.php
+++ resources/views/reservation/visits.blade.php
@@ -35,8 +35,8 @@
 {{--//        return ['日', '一', '二', '三', '四', '五', '六'][$weekday];--}}
 {{--//    }--}}
 {{--//--}}
-    <form action="{{route('visits.index')}}" method="POST" role="form">
-        {{ method_field('GET') }}
+    <form action="{{route('visit.store')}}" method="POST" role="form">
+        {{ method_field('POST') }}
         {{ csrf_field() }}
         {{--    @foreach($posts as $post)--}}
         {{--        <tr><td><div>{{ $post->title }}</div></td></tr>--}}
