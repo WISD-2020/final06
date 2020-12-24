748156dc362fd5e28381065aeb02129490dfab0b
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-24 18:09:06 +0800

    history model

---
+++ app/Models/History.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    protected $table = 'museum_histories';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+}
