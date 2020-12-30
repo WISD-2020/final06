d4d7b833abe9b76289cb5845224036f740f98b4d
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-31 02:00:20 +0800

    修改後台預約參訪controller

--- app/Admin/Controllers/VisitsController.php
+++ app/Admin/Controllers/VisitsController.php
@@ -31,6 +31,7 @@ class VisitsController extends AdminController
         $grid->column('date', __('Date'));
         $grid->column('period', __('Period'));
         $grid->column('way_id', __('Way id'));
+        $grid->column('result', __('Result'));
 
         return $grid;
     }
@@ -50,6 +51,7 @@ class VisitsController extends AdminController
         $show->field('date', __('Date'));
         $show->field('period', __('Period'));
         $show->field('way_id', __('Way id'));
+        $show->field('result', __('Result'));
 
         return $show;
     }
@@ -69,6 +71,7 @@ class VisitsController extends AdminController
         $form->select('period',__('Period'))->options(['10:00-12:00' => '10:00-12:00', '11:00-13:00' => '11:00-13:00','12:00-14:00' => '12:00-14:00'
         ,'13:00-15:00' => '13:00-15:00','14:00-16:00' => '14:00-16:00','15:00-17:00' => '15:00-17:00','16:00-18:00' => '16:00-18:00', 'val' => 'period']);
         $form->number('way_id', __('Way id'));
+        $form->text('result', __('Result'))->default('未確認');
 
         return $form;
     }
