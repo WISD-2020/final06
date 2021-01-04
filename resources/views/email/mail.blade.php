8d53d0ba3bb46904bf2220cce0efad38bd452560
li ya hsuan <ab53014698@yahoo.com.tw>
2021-01-04 21:53:18 +0800

    修改寄送email的內容

--- resources/views/email/mail.blade.php
+++ resources/views/email/mail.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>send email</title>
+    <title>寄送email</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="assets/css/main.css" />
 </head>
 <body>
-    <h3>Hello, {{$name}}</h3>
-    <h4>郵件內容:</h4>
+    <h3>你好, {{$name}}</h3>
+    <h4>email內容:</h4>
     <p>{!! $content !!}</p>
 </body>
 </html>
