6b942ab60abbb17c4f649784cfe0124ace59bb46
chang ting yu <s3a732009@student.ncut.edu.tw>
2021-01-06 00:50:36 +0800

    修改錯誤

--- app/Admin/Controllers/VisitsController.php
+++ app/Admin/Controllers/VisitsController.php
@@ -71,7 +71,7 @@ class VisitsController extends AdminController
         $form->select('period',__('Period'))->options(['10:00-12:00' => '10:00-12:00', '11:00-13:00' => '11:00-13:00','12:00-14:00' => '12:00-14:00'
         ,'13:00-15:00' => '13:00-15:00','14:00-16:00' => '14:00-16:00','15:00-17:00' => '15:00-17:00','16:00-18:00' => '16:00-18:00', 'val' => 'period']);
         $form->number('way_id', __('Way id'));
-        $form->text('result', __('Result'))->default('未確認');
+        $form->text('result', __('Result'))->default('0');
 
         return $form;
     }
